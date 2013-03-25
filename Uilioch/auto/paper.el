3df2c1f9d1670c1806836dc525f519ae9df73f80
Joseph R. Kiniry <kiniry@acm.org>
2013-03-25 15:34:35 +0100

    Finished rewrite of VoteID paper.

---
+++ Uilioch/auto/paper.el
@@ -0,0 +1,161 @@
+(TeX-add-style-hook "paper"
+ (lambda ()
+    (TeX-add-symbols
+     '("myhref" 2)
+     '("tiebreak" 1)
+     '("ballotfour" 4)
+     '("ballotthree" 3)
+     '("ballottwo" 2)
+     '("ballotone" 1)
+     "eg"
+     "ie"
+     "etc"
+     "votail"
+     "eireann"
+     "dail"
+     "outcome"
+     "scenario"
+     "ballots"
+     "ballotspec"
+     "config"
+     "candidates"
+     "election"
+     "winners"
+     "losers"
+     "winner"
+     "earlyloser"
+     "earlyloserNT"
+     "loser"
+     "quota"
+     "belowthreshold"
+     "earlybelowthreshold"
+     "earlybelowthresholdNT"
+     "quotawinnernontransferable"
+     "winnernontransferable"
+     "abovequotawinner"
+     "highwinner"
+     "surpluswinner"
+     "first"
+     "second"
+     "third")
+    (TeX-add-symbols
+     '("myhref" 2)
+     '("tiebreak" 1)
+     '("ballotfour" 4)
+     '("ballotthree" 3)
+     '("ballottwo" 2)
+     '("ballotone" 1)
+     "eg"
+     "ie"
+     "etc"
+     "votail"
+     "eireann"
+     "dail"
+     "outcome"
+     "scenario"
+     "ballots"
+     "ballotspec"
+     "config"
+     "candidates"
+     "election"
+     "winners"
+     "losers"
+     "winner"
+     "earlyloser"
+     "earlyloserNT"
+     "loser"
+     "quota"
+     "belowthreshold"
+     "earlybelowthreshold"
+     "earlybelowthresholdNT"
+     "quotawinnernontransferable"
+     "winnernontransferable"
+     "abovequotawinner"
+     "highwinner"
+     "surpluswinner"
+     "first"
+     "second"
+     "third")
+    (LaTeX-add-bibliographies
+     "abbrev"
+     "dissertation")
+    (LaTeX-add-environments
+     "Lemma"
+     "Definition"
+     "Theorem")
+    (LaTeX-add-labels
+     "sec:theory-pref-ball"
+     "subsec:mathematical-models"
+     "subsec:plurality"
+     "sec:proc-autom-test"
+     "sec:results-concl-future"
+     "sec:methodology")
+    (TeX-add-symbols
+     '("myhref" 2)
+     '("tiebreak" 1)
+     '("ballotfour" 4)
+     '("ballotthree" 3)
+     '("ballottwo" 2)
+     '("ballotone" 1)
+     "eg"
+     "ie"
+     "etc"
+     "votail"
+     "eireann"
+     "dail"
+     "outcome"
+     "scenario"
+     "ballots"
+     "ballotspec"
+     "config"
+     "candidates"
+     "election"
+     "winners"
+     "losers"
+     "winner"
+     "earlyloser"
+     "earlyloserNT"
+     "loser"
+     "quota"
+     "belowthreshold"
+     "earlybelowthreshold"
+     "earlybelowthresholdNT"
+     "quotawinnernontransferable"
+     "winnernontransferable"
+     "abovequotawinner"
+     "highwinner"
+     "surpluswinner"
+     "first"
+     "second"
+     "third")
+    (TeX-run-style-hooks
+     "xspace"
+     "hyperref"
+     "pdfauthor=\"MysteryZeroandMysteryOne\""
+     "urlcolor=blue"
+     "citecolor=blue"
+     "linkcolor=blue"
+     "pdfstartview=FitV"
+     "colorlinks=true"
+     "naturalnames=true"
+     "plainpages=false"
+     "bookmarks=false"
+     "pdftex"
+     "pseudocode"
+     "ntheorem"
+     "listings"
+     "float"
+     "eucal"
+     "amsfonts"
+     "amsmath"
+     "epsfig"
+     "tabularx"
+     "longtable"
+     "eurosym"
+     ""
+     "latex2e"
+     "llncs10"
+     "llncs"
+     "a4paper"
+     "runningheads")))
+
